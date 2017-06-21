f51d1484279853282d9f19e20155b0afdf27d28f
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-06-21 21:08:51 +0200

    adds tests

---
+++ src/services/predictor.test.ts
@@ -0,0 +1,81 @@
+// Imports
+import { expect } from 'chai';
+import * as co from 'co';
+import 'mocha';
+import * as sinon from 'sinon';
+
+// Imports services
+import { PredictorService } from './predictor';
+
+// Imports repositories
+// import { MockConsumerRepository } from './../repositories/mock/consumer';
+
+// Imports domain models
+import { Entity } from './../models/entity';
+import { Word } from './../models/word';
+
+describe('PredictorService', () => {
+
+    describe('toWords', () => {
+
+        let predictorService: PredictorService = null;
+
+        beforeEach(() => {
+            
+            predictorService = new PredictorService(null);
+        });
+
+        it('should return list of words with frequency', () => {
+
+            return co(function*() {
+                const result: Word[] = predictorService.toWords('<p>Hello World.</p><p>I wonder if the world would reply?</p>')
+                
+                expect(result.length).to.be.eq(4);
+                expect(result[0].value).to.be.eq('hello');
+                expect(result[1].value).to.be.eq('world');
+                expect(result[2].value).to.be.eq('wonder');
+                expect(result[3].value).to.be.eq('reply');
+
+                expect(result[0].frequency).to.be.eq(1);
+                expect(result[1].frequency).to.be.eq(2);
+                expect(result[2].frequency).to.be.eq(1);
+                expect(result[3].frequency).to.be.eq(1);
+            });
+        });
+    });
+
+    describe('joinWords', () => {
+
+        let predictorService: PredictorService = null;
+
+        beforeEach(() => {
+            
+            predictorService = new PredictorService(null);
+        });
+
+        it('should return list of words with combined frequency', () => {
+
+            return co(function*() {
+                const result: Word[] = predictorService.joinWords(
+                    [
+                        new Word('hello', 1),
+                        new Word('world', 3)
+                    ],
+                    [
+                        new Word('hello', 1),
+                        new Word('stuff', 5)
+                    ]
+                );
+                
+                expect(result.length).to.be.eq(3);
+                expect(result[0].value).to.be.eq('hello');
+                expect(result[1].value).to.be.eq('world');
+                expect(result[2].value).to.be.eq('stuff');
+
+                expect(result[0].frequency).to.be.eq(2);
+                expect(result[1].frequency).to.be.eq(3);
+                expect(result[2].frequency).to.be.eq(5);
+            });
+        });
+    });
+});
